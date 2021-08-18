990c70fe78f284b34cec95e86d6451b8620c05a6
Luana <luana.nadai.santos@gmail.com>
2021-08-17 22:31:25 -0300

    Arquivos API criados para usar fake API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
+import React, {Component} from 'react';
 import CharacterCard from './components/CharacterCard.js'
+import ApiDbd from './api/api.js'
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Oi</h1>
-      <CharacterCard name='Dwight' difficulty='Easy' />
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    survivalList: [],
+    killerList: [],
+    perkList: []
+  }
+
+  componentDidMount() {
+    ApiDbd.survival().then((result) => {
+      this.setState({
+        survivalList: result.data
+      })
+    });
+    
+    ApiDbd.killer().then((result) => {
+      this.setState({
+        killerList: result.data
+      })
+    });
+
+    ApiDbd.perks().then((result) => {
+      this.setState({
+        perkList: result.data
+      })
+    });
+  }
+  
+  render() {
+    return (
+      <>
+        <div className="App">
+          <h1>Oi</h1>
+        </div>
+      </>
+    );
+  };
 }
 
 export default App;
