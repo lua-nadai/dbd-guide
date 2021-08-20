4d4980c7620781e154168dab4275c5fc57cf544d
Luana <luana.nadai.santos@gmail.com>
2021-08-20 17:42:59 -0300

    Alteracao logo navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,9 @@ import {Link} from 'react-router-dom'
 const Navbar = () => {
     return (
         <nav className='navbar'>
-            <Link to='/'><img src='../../public/images/dbd-logo.png' alt='DBD logo' /></Link>
+            <Link to='/'>
+                <img src='https://logos-world.net/wp-content/uploads/2021/02/Dead-by-Daylight-Symbol.jpg' alt='DBD logo' width='90'/>
+            </Link>
             <Link to='/survivors'>Survivors</Link>
             <Link to='/killers'>Killers</Link>
             <Link to='/perks'>Perks</Link>
