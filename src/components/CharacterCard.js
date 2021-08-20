7e95de23f33e51143a673303a9cf7f072aea8db8
Luana <luana.nadai.santos@gmail.com>
2021-08-20 17:24:47 -0300

    SearchPerk arrumado e alteraçoes no estilo do header nas rotas

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -4,7 +4,7 @@ const CharacterCard = (props) => {
     return (
         <div className='character-card'>
             <img src={`${props.icon.preview_portrait}`} alt={props.name} width='150' />
-            <h3>Name: {props.name}</h3>
+            <h3>{props.name}</h3>
             <h3>Difficulty: {props.difficulty}</h3>
         </div>
     )
