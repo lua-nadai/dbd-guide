20ff3b3fb626769ceb29cf4243ea61266caccccf
Luana <luana.nadai.santos@gmail.com>
2021-08-20 17:12:51 -0300

    Componente PerkList criado

---
+++ src/components/PerkList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+import PerkCard from './PerkCard.js';
+import Search from './Search.js';
+
+class PerkList extends Component {
+    render() {
+        return(
+            <>
+                <h1>Perks</h1>
+                <Search characterFilter={this.props.characterFilter}/>
+                <div className='character-list'>
+                    {this.props.perks.map( perk => 
+                        <Link to={`/perks/${perk._id}`}> 
+                            <PerkCard {...perk}/> 
+                        </Link>
+                    )}
+                </div>
+            </>
+        )
+    }
+}
+
+export default PerkList
