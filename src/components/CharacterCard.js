7032c464436b307ff5f8516e787276d5fa489915
Luana <luana.nadai.santos@gmail.com>
2021-08-20 10:47:58 -0300

    Componente CharacterList criado

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 const CharacterCard = (props) => {
     return (
         <div className='character-card'>
-            <img src={props.preview_portrait} alt={props.name}/>
+            <img src={`${props.icon.preview_portrait}`} alt={props.name} width='150' />
             <h3>Name: {props.name}</h3>
             <h3>Difficulty: {props.difficulty}</h3>
         </div>
---
+++ src/components/CharacterList.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+import CharacterCard from "./CharacterCard";
+
+
+class CharacterList extends Component {
+
+    render () {
+        return (
+            <div>
+                {this.props.characters.map(character => <Link to={`${this.props.characterType}/${character._id}`} key = {character._id} > <CharacterCard {...character} /> </Link> )}
+            </div>
+        )
+    }
+}
+
+export default CharacterList
