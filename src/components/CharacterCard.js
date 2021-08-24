949cbeedefa3db6d192ba9319f6ec1e310f9a13d
Luana <luana.nadai.santos@gmail.com>
2021-08-24 19:38:04 -0300

    Componente HomeInfo criada

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  border: 1px solid black;
+  width: 20vw;
+  height: 28vh;
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
 
 const CharacterCard = (props) => {
     return (
-        <div className='character-card'>
+        <Card>
             <img src={`${props.icon.preview_portrait}`} alt={props.name} width='150' />
             <h3>{props.name}</h3>
             <h3>Difficulty: {props.difficulty}</h3>
-        </div>
+        </Card>
     )
 }
 
