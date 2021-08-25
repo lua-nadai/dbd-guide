b6371789dbf37c0d6682978df85c83c4df202e95
Luana <luana.nadai.santos@gmail.com>
2021-08-24 22:34:10 -0300

    Informaçoes do Perk incluidas como Tooltip

--- src/components/PerkCard.js
+++ src/components/PerkCard.js
@@ -1,22 +1,29 @@
 import React from 'react';
 
+
 export const PerkCard = (props) => {
     return (
-        <div className='character-card'>
-            <img src={`${props.icon}`} alt={props.name} width='180'/>
-            <h3>{props.perk_name}</h3>
-        </div>
+        <>        
+            <div className="perk-card" data-toggle="tooltip"
+                title={
+                    `-  
+        Role: ${props.role}
+
+        Character Name: ${props.name}
+
+        Perk Name: ${props.perk_name}
+        
+        Description: ${props.description}`}>
+
+                <img src={`${props.icon}`} alt={props.name} width='180' />
+                <h3>{props.perk_name}</h3>
+
+
+            </div>
+            
+        </>
     )
 }
 
-export const PerkInfo = (props) => {
-    return (
-        <div>
-            <span>Role: {props.role}</span>
-            <span>Character Name: {props.name}</span>
-            <span>Perk Name: {props.perk_name}</span>
-            <span>Description: {props.description}</span>
-        </div>
-    )
-}
+
 
