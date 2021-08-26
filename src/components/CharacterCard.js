a58821a49db37e0380e32139322ba5f7a6b74a98
Luana <luana.nadai.santos@gmail.com>
2021-08-25 21:35:26 -0300

    Alteracoes CSS na HomePage e CharactersList

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import styled from 'styled-components';
 
-const Card = styled.div`
-  border: 1px solid black;
-  width: 20vw;
-  height: 28vh;
-  margin: 10px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
+import {
+    Card, CardTitle, CardText
+} from 'reactstrap';
 
 const CharacterCard = (props) => {
     return (
-        <Card>
-            <img src={`${props.icon.preview_portrait}`} alt={props.name} width='150' />
-            <h3>{props.name}</h3>
-            <h3>Difficulty: {props.difficulty}</h3>
+        <Card className='character-card rounded' body inverse style = {{ backgroundColor: '#333', borderColor: '#333' }}>
+
+            <img src={`${props.icon.preview_portrait}`} alt={props.name} width='150'/>
+
+            <CardTitle tag="h5">{props.name}</CardTitle>
+            <CardText>Difficulty: {props.difficulty}</CardText>
         </Card>
     )
 }
