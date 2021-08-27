8af944d14312e4f4837c6d5c36bf89540d1c44b9
Luana <luana.nadai.santos@gmail.com>
2021-08-27 00:11:50 -0300

    Troca do Tooltip para Modal nos Cards de Perk

--- src/components/PerkCard.js
+++ src/components/PerkCard.js
@@ -1,25 +1,39 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 export const PerkCard = (props) => {
+
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => setModal(!modal);
+
+
     return (
-        <>  
-            <div className='perk-format'>
-                <div className="perk-card" data-toggle="tooltip"
-                    title={
-                        `-  
-            Role: ${props.role}
-
-            Character Name: ${props.name}
-
-            Perk Name: ${props.perk_name}
-            
-            Description: ${props.description}`}>
-
+        <>
+            <Button className='perk-format' onClick={toggle}>
+                
+                <div className="perk-card" >
                     <img src={`${props.icon}`} alt={props.name} width='180' />
                     <h3>{props.perk_name}</h3>
                 </div>
-            </div>
+            </Button>
+                <div>
+
+                <Modal className="modal-dialog modal-lg" isOpen={modal} toggle={toggle}  >
+                    <ModalHeader><h1>{props.perk_name}</h1></ModalHeader>
+                        <ModalBody>
+                            <h2>Role: {props.role}</h2><br/>
+                            <h2>Perk Name: {props.perk_name}</h2><br/>
+                            <h2>Character Name: <a href='#'>{props.name}</a></h2><br/>
+                            <h2>Teach Leve: {props.teach_level}</h2><br/>
+                        <h2>Description:<br/>{props.description}</h2><br/>
+                        </ModalBody>
+                    <ModalFooter>
+                        <Button color="secondary" onClick={toggle}>Close</Button>
+                    </ModalFooter>
+                 </Modal>
+                </div>
+
         </>
     )
 }
