cf32fac5273978a0e867ea127c783c2b138a3a34
Luana <luana.nadai.santos@gmail.com>
2021-08-27 19:13:41 -0300

    Alteracao no estilo da pagina de Personagens

--- src/components/SurvivalInfo.js
+++ src/components/SurvivalInfo.js
@@ -4,15 +4,27 @@ import ApiDbd from '../api/api.js';
 
 class SurvivalInfo extends Component {
     state = {
-        character: {}
+        character: {},
+        perks: {}
     }
     
-    componentDidMount = async () => {       
-        const result = await ApiDbd.oneSurvival(this.props.match.params._id)
+    componentDidMount = async () => {   
+
+        const survival = await ApiDbd.oneSurvival(this.props.match.params._id)
+        const perks = await ApiDbd.perks()
 
         this.setState({
-            character: result.data
+            character: survival.data,
+            perks: perks.data
         })       
+    }
+
+    findPerk = (perk, character) => {
+        const characterPerks = {}
+
+        if(perk.name === character.name){
+            return this.state.perk_name
+        }
     }
     
     render(){
@@ -25,30 +37,41 @@ class SurvivalInfo extends Component {
 
                         <div className='home-page'>
                             
-                            <h1 className='center'>{this.state.character.name}</h1>
-                            
-                            <div className='center'>
-                                {<img src={`${this.state.character.icon.portrait}`} alt={`${this.state.character.name} portrait`} width='150' />}
+                            <h1 className='character-title center'>{this.state.character.name}</h1>
 
-                                <div className='character-box'>
-                                    <h3>{this.state.character.full_name}</h3>
-                                    <p>Gender: {this.state.character.gender}</p>
-                                    <p>Role: {this.state.character.role}</p>
-                                    <p>Nationality: {this.state.character.nationality}</p>
-                                    <p>DLC: {this.state.character.dlc}</p>
-                                </div>
-                            </div>
+                            <section className='section-character'>
+                                <div className='character-container-perk' >
+                                    {console.log(this.findPerk('Adrenaline', 'Meg Thomas'))}
 
-                            <div className='center'>
-                                <div className='character-perk' >
-                                    {this.state.character.perks.map( (perk, index) => <h1 index>{perk}</h1> )}
+                                    {this.state.character.perks.map((perk, index) => 
+                                        <div className='character-perk'>
+                                            <img/>
+                                            <h1 index>{perk}</h1> 
+                                            <h2>Teach Level: {}</h2>
+                                        </div> 
+                                    )}
+                                    
                                 </div>
 
+                                <div className='center character-info'>
+                                    {<img src={`${this.state.character.icon.portrait}`} alt={`${this.state.character.name} portrait`} width='150' />}
+
+                                    <div className='character-box'>
+                                        <h3>{this.state.character.full_name}</h3>
+                                        <p>Gender: {this.state.character.gender}</p>
+                                        <p>Role: {this.state.character.role}</p>
+                                        <p>Nationality: {this.state.character.nationality}</p>
+                                        <p>DLC: {this.state.character.dlc}</p>
+                                    </div>
+                                </div>
+                            </section>
+
+                            <section className='center'>
                                 <div className='character-story'>
-                                    <h2>Story:</h2>
+                                    <h1>Story:</h1>
                                     <p>{this.state.character.lore}</p>
                                 </div>
-                            </div>
+                            </section>
                         </div>
                     </div>:<h1>Loading...</h1>
                 }
